7aa0404f9c8156bb343fdf7a0b2af74cdeae285a
ddtime123 <aa2797559967@gmail.com>
2021-10-14 21:50:57 +0800

    commit new changes

--- src/main/java/tacos/HomeController.java
+++ src/main/java/tacos/HomeController.java
@@ -1,13 +1,23 @@
 package tacos;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
 
-    @GetMapping("/")
-    public String home(){
+    @GetMapping("/hello")
+    public String home(@RequestParam(name="http_name",required=true) String name, Model model){
+        model.addAttribute("name",name);
         return "home";
+    }
+
+    @GetMapping("/Index")
+    public String index(@RequestParam(name="http_name",required=true) String name, Model model){
+        return "index";
     }
 }
